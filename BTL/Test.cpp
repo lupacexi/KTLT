4078537873dcf32b7f6d45697250096ca5ef5622
lupacexi <lupacexi@gmail.com>
2017-04-07 20:19:05 +0700

    07/04/2017

---
+++ BTL/Test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+using namespace std;
+
+int main(){
+	int x;
+	printf("Hello World\n");
+	cin>>x;
+	cout<<"x="<<x<<endl;
+	return 0;
+}
