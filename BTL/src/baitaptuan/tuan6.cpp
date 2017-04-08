e5a7d42da81f4267c8a085abffee815a8d324abc
lupacexi <lupacexi@gmail.com>
2017-04-08 15:03:39 +0700

    bai6

---
+++ BTL/src/baitaptuan/tuan6.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+typedef struct Node{
+	int data;
+	struct Node* pre;
+	struct Node* next;
+}Node;
+
+typedef struct Stack{
+	Node *head;
+	int max;
+	int count;
+	int top;
+};
+
+Node *Create(){
+
+}
+
+
+int main(){
+
+
+	return 0;
+}
+
+
+
